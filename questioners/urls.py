f61b330f6c1d3f3e8b19342206739a8700483f0d
Anish <GA352491@gmail.com>
2024-02-26 13:55:01 +0530

    modified code

--- questioners/urls.py
+++ questioners/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from questioners.views import QuestionAPI
+from questioners.views import QuestionAPI,QuestionEachAPI
 
 urlpatterns = [
     path('questions/', QuestionAPI.as_view()),
+    path('question/', QuestionEachAPI.as_view()),
 ]
--- questioners/views.py
+++ questioners/views.py
@@ -8,6 +8,7 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate, login
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.pagination import PageNumberPagination
 
 
 # Create your views here.
@@ -17,3 +18,25 @@ class QuestionAPI(APIView):
         questions = Question.objects.all()
         question_serializer = QuestionSerializer(questions, many=True)
         return Response({'data': question_serializer.data})
+
+
+from rest_framework import pagination
+
+
+class MyPageNumberPagination(pagination.PageNumberPagination):
+    page_size = 1
+    page_size_query_param = 'limit'
+
+
+class QuestionEachAPI(APIView, MyPageNumberPagination):
+    page_size = 1
+    serializer_class = QuestionSerializer
+
+    def get(self, request, format=None):
+        try:
+            questions_objs = Question.objects.all()
+            results = self.paginate_queryset(questions_objs, request, view=self)
+            questionsserializer = self.serializer_class(results, many=True)
+        except Exception as e:
+            return Response(str(e))
+        return self.get_paginated_response(questionsserializer.data)

8c9b38b7b9ac56b9fdd1363c97ec49e51ea22a3f
ps018w <46179440+ps018w@users.noreply.github.com>
2024-02-27 11:44:38 +0530

    Update settings.py

--- QuizAus/settings.py
+++ QuizAus/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
+STATIC_ROOT = os.path.join(BASE_DIR, '.staticfiles')
+STATIC_URL = '/static/'
+MEDIA_URL = 'media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

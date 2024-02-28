686e936149919933c4f96579fdc30508a076c091
ps018w <46179440+ps018w@users.noreply.github.com>
2024-02-28 10:19:56 +0530

    Update settings.py

--- QuizAus/settings.py
+++ QuizAus/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a%+!!-x0qb6vey6n^@-xij&jxcs=eua_#mr71iz0498wj!r+^#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
